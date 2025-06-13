ddda15aa7955bc6dd2d112ef374c7d0434c81ff2
srini41 <srinikesh.singarapu@gmail.com>
2025-06-12 23:41:02 -0400

    Add missing Document import from langchain-core

--- utils/document_loader.py
+++ utils/document_loader.py
@@ -5,6 +5,7 @@ from typing import List
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
+from langchain_core.documents import Document
 import json
 import numpy as np
 
